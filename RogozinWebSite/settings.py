9917ff59095c4bd014c6868562a91e2d86620e9f
bushuev-alex <bushuev-alex@mail.ru>
2023-02-24 20:36:49 +0700

    1. remove secret key

--- RogozinWebSite/settings.py
+++ RogozinWebSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from key import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ju!jhq@!vi7hb_i^adzs1e8=ag07kc$%z7y1a+(p_k!k$1%_b^"
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
