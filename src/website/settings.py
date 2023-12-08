557fda85480f31ad0287893e1892de91009d9ced
bushuev-alex <bushuev-alex@mail.ru>
2023-12-09 05:42:48 +0700

    set debug = False

--- src/website/settings.py
+++ src/website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = DJ_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["212.20.45.230", "127.0.0.1", "192.168.0.16", "192.168.0.126", "localhost",
                  "grigoriyrogozin.ru", "www.grigoriyrogozin.ru", "0.0.0.0"]
