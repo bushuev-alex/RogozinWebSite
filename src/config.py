4725d60bd00edf3cb1c586191a6a44913ec0f45a
bushuev-alex <bushuev-alex@mail.ru>
2023-12-09 00:30:36 +0700

    add config.py

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_LOGIN = os.getenv('DB_LOGIN')
+DB_PASS = os.getenv('DB_PASS')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+UVCRN_HOST = os.getenv('UVCRN_HOST')
+UVCRN_PORT = os.getenv('UVCRN_PORT')
+SSL_KEYFILE = os.getenv('SSL_KEYFILE')
+SSL_SERTIF = os.getenv('SSL_SERTIF')
