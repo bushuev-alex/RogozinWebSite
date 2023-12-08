bdf415377b1cedb8b7fcc515912062b1a5d9d2ad
bushuev-alex <bushuev-alex@mail.ru>
2023-12-09 03:03:42 +0700

    remove sys path to wsgi

--- src/website/wsgi.py
+++ src/website/wsgi.py
@@ -12,7 +12,7 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '.'))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
