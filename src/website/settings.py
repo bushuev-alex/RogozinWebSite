ec0489cdca2844c271ae0e8cdcd72198eb27e5d0
bushuev-alex <bushuev-alex@mail.ru>
2023-12-09 01:02:48 +0700

    add env keys to posgres db

--- src/website/settings.py
+++ src/website/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
-from config import DJ_SECRET
+from config import DJ_SECRET, DB_NAME, DB_LOGIN, DB_PASS, DB_HOST, DB_PORT
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "website.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": DB_NAME,
+        'USER': DB_LOGIN,
+        'PASSWORD': DB_PASS,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
