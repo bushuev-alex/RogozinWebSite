c27b60c1083ee4f2d534c3e554a4029c0c93928f
bushuev-alex <bushuev-alex@mail.ru>
2023-12-09 20:32:17 +0700

    register prose models in admin.py

--- src/prose/admin.py
+++ src/prose/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from prose.models import ProjitoeNeUgasaet, ThoughtAboutLived, PagesOfHistory
+
+admin.site.register(ProjitoeNeUgasaet)
+admin.site.register(ThoughtAboutLived)
+admin.site.register(PagesOfHistory)
