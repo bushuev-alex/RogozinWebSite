ea7914ba0f54f10171250f365628ee11a0a86b60
bushuev-alex <bushuev-alex@mail.ru>
2023-12-10 11:48:38 +0700

    add poetry admin blogadmin with summernote modeladmin

--- src/poetry/admin.py
+++ src/poetry/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from poetry.models import ChildPoetry
+
+
+class blogadmin(SummernoteModelAdmin):
+    # displaying posts with title slug and created time
+    list_display = ('title', 'text')
+    list_filter = ("title", )
+    search_fields = ['title', 'text']
+    # prepopulating slug from title
+    # prepopulated_fields = {'slug': ('title', )}
+    summernote_fields = ('text', )
+
 
 # Register your models here.
+admin.site.register(ChildPoetry, blogadmin)
